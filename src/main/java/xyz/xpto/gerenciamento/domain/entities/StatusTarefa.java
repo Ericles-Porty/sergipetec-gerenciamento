976d58861e4c6514e9d0c87f51fde931ec4cad9e
Ericles <ericlesdsantos@gmail.com>
2025-02-17 14:44:26 -0300

    feat: add StatusTarefa entity and associate it with Tarefa for task status management

--- src/main/java/xyz/xpto/gerenciamento/domain/entities/StatusTarefa.java
+++ src/main/java/xyz/xpto/gerenciamento/domain/entities/StatusTarefa.java
@@ -1,12 +1,13 @@
 package xyz.xpto.gerenciamento.domain.entities;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -15,34 +16,21 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "tarefa")
+@Table(name = "status_tarefa")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Tarefa {
+public class StatusTarefa {
     @Id
-    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "tarefa_id_sequence")
     @SequenceGenerator(name = "tarefa_id_sequence", sequenceName = "tarefa_id_sequence", allocationSize = 1)
+    @Column(name = "id")
     private long id;
 
-    @Column(name = "titulo", nullable = false, length = 100)
-    private String titulo;
+    @Column(name = "nome", nullable = false)
+    private String nome;
 
-    @Column(name = "descricao", nullable = true)
-    private String descricao;
-
-    @ManyToOne
-    @JoinColumn(name = "projeto_id", nullable = false)
-    private Projeto projeto;
-
-    @ManyToOne
-    @JoinColumn(name = "pessoa_id", nullable = true)
-    private Pessoa responsavel;
-
-    @ManyToOne
-    @JoinColumn(name = "status_tarefa_id", nullable = false)
-    private StatusTarefa statusTarefa;
-
+    @OneToMany(mappedBy = "statusTarefa", targetEntity = Tarefa.class)
+    private List<Tarefa> tarefas;
 }

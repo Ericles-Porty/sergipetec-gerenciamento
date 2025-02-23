839861a1f5efdb43f8dff543ecc03f617f7f9f55
Ericles <ericlesdsantos@gmail.com>
2025-02-23 17:10:35 -0300

    refact: simplify ObterPessoa DTO by removing unused JsonInclude annotation and reformatting code

--- src/main/java/xyz/xpto/gerenciamento/application/services/pessoa/dtos/ObterPessoa.java
+++ src/main/java/xyz/xpto/gerenciamento/application/services/pessoa/dtos/ObterPessoa.java
@@ -1,20 +1,17 @@
 package xyz.xpto.gerenciamento.application.services.pessoa.dtos;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-
 import lombok.Builder;
 
 @Builder
 public record ObterPessoa() {
-    public record Request(long id) {}
+	public record Request(long id) {}
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    @Builder
-    public record Response(
-            long id,
-            String nome,
-            EquipeResponse equipe) {
-        public record EquipeResponse(long id, String nome) {}
-    }
+	@Builder
+	public record Response(
+			long id,
+			String nome,
+			EquipeResponse equipe) {
+		public record EquipeResponse(long id, String nome) {}
+	}
 
 }
